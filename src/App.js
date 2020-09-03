34b6d99c9440326feab26171825173b65c4e0742
Nada Ourir <Nada.Ourir@se.linedata.com>
2020-09-03 17:38:32 +0100

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react'
 import Quiz from './Quiz.js'
 import './App.css'
-import quiz_questions from'./quiz_data.json'
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Quiz quiz_questions = {quiz_questions} />
-      </div>
+        <Quiz />
     )
   }
 }
