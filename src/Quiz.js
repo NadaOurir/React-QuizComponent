2a898c04ddec68b0c865b312e65f1c4b66ab51d8
Nada Ourir <Nada.Ourir@se.linedata.com>
2020-09-03 17:30:03 +0100

    Update Quiz.js
    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,9 +11,9 @@ class Quiz extends Component {
 
   render() {
     return (
-      <div>
-          <div className='QuizQuestion'>{this.props.quiz_questions.quiz_questions[0].instruction_text }</div>
-      </div>
+
+      <div className='QuizQuestion'>{this.props.quiz_questions.quiz_questions[0].instruction_text }</div>
+
     )
   }
 }
